5e1539a80851c8c0a255898de1222c1f168aefc6
haeuncs <haeun.developer@gmail.com>
2020-11-06 23:23:50 +0900

    :lipstick: Start Filter List Design 2

--- RetroEditor/RetroEditor/PhotoEdit/FilterListView.swift
+++ RetroEditor/RetroEditor/PhotoEdit/FilterListView.swift
@@ -7,18 +7,66 @@
 
 import SwiftUI
 
+struct FilterCell: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Image("icnFish")
+                .resizable()
+                .padding(EdgeInsets(
+                            top: 13,
+                            leading: 13,
+                            bottom: 13,
+                            trailing: 13
+                ))
+            HStack {
+                Text("test")
+            }
+            .frame(
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .center
+            )
+            .windowsBorder(reverse: true)
+        }
+        .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .center
+        )
+        .background(Color.Retro.gray4)
+        .windowsBorder()
+    }
+}
+
+
+struct StickerCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterCell()
+    }
+}
+
 struct FilterListView: View {
-    private var symbols = ["keyboard", "hifispeaker.fill", "printer.fill", "tv.fill", "desktopcomputer", "headphones", "tv.music.note", "mic", "plus.bubble", "video"]
-    private var gridItemLayout = [GridItem(.fixed(90))]
+    enum Constant {
+        static let gridItem = GridItem(.fixed(90))
+        static let length: CGFloat = 90
+    }
 
     var body: some View {
         ScrollView(.horizontal) {
-            LazyHGrid(rows: gridItemLayout, spacing: 20) {
-                ForEach(symbols, id: \.self) { item in
-                    Text(item)
+            LazyHGrid(rows: [Constant.gridItem], spacing: 0) {
+                ForEach((0...999), id: \.self) { _ in
+                    FilterCell()
+                        .frame(
+                            width: Constant.length,
+                            height: Constant.length,
+                            alignment: .center
+                        )
                 }
             }
         }
+        .windowsBorder()
         .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 90)
     }
 }
